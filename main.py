f4ef29816f005eb88170675a9d8b03df0d8846e4
LuisPHernandez <lphm0105@gmail.com>
2025-06-17 12:31:11 -0600

    login and signup

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from authorization import verify_token
+from fastapi import FastAPI, HTTPException, Body
+from pydantic import BaseModel, EmailStr
+from authorization import create_token, verify_token
 from proximity import is_near_ble_beacon
+from passlib.context import CryptContext
 
 app = FastAPI()
 
+# Request Models
 class GateOpenRequest(BaseModel):
     token: str
-    gate_id: str  # extracted from scanned QR
+    gate_id: str
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class SignupRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+# Simple in-memory "database"
+users_db = {}
+
+# Password hasher
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 @app.get("/")
 def root():
     return {"message": "API funcionando"}
 
+@app.post("/login")
+def login(request: LoginRequest):
+    email = request.email
+    password = request.password
+    if (email not in users_db):
+        raise HTTPException(status_code=401, detail="User not found")
+    if (not pwd_context.verify(password, users_db[email])):
+        raise HTTPException(status_code=401, detail="Incorrect password")
+    
+    token = create_token(email)
+    return {"token": token}
+
+@app.post("/signup")
+def login(request: SignupRequest):
+    if request.email in users_db:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    hashed_password = pwd_context.hash(request.password)
+    users_db[request.email] = hashed_password
+
+    return {"message": "Signup successful"}
+
+@app.post("/open_gate")
 async def open_gate(request: GateOpenRequest):
     try:
         decoded = verify_token(request.token)
 
-        user_id = decoded["user_id"]
+        user_id = decoded["email"]
+        exp_time = decoded["exp"]    
 
         if not is_near_ble_beacon(user_id):
             raise HTTPException(status_code=403, detail="User not near gate")
@@ -25,7 +65,7 @@ async def open_gate(request: GateOpenRequest):
         # TODO Send signal to microcontroller
         print(f"Opening gate {request.gate_id} for user {user_id}")
 
-        return {"status": "success", "user_id": user_id, "gate_id": request.gate_id}
+        return {"user_id": user_id, "gate_id": request.gate_id}
 
     except Exception as e:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
