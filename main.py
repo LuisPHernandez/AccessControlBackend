f019f3a2d6ff823ff62bc7e8e6ec5e1155bb1a87
LuisPHernandez <lphm0105@gmail.com>
2025-06-26 14:46:49 -0600

    Modify login and signup for PostgreSQL integration

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr
 from authorization import create_token, verify_token
 from gate_controller import send_open_message
-from proximity import is_near_ble_beacon
 from passlib.context import CryptContext
+from db import get_db_connection
+
+# Global variables
+IP_MICRO = "192.168.0.60"
 
 app = FastAPI()
-IP_MICRO = "192.168.0.60"
 
 # Request Models
 class GateOpenRequest(BaseModel):
@@ -21,9 +23,6 @@ class SignupRequest(BaseModel):
     email: EmailStr
     password: str
 
-# Simple in-memory "database"
-users_db = {}
-
 # Password hasher
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -33,24 +32,75 @@ def root():
 
 @app.post("/login")
 def login(request: LoginRequest):
+    # Connect to the database
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # Extract email and password from request
     email = request.email
     password = request.password
-    if (email not in users_db):
-        raise HTTPException(status_code=401, detail="User not found")
-    if (not pwd_context.verify(password, users_db[email])):
+
+    # Check if a user exists with the given email
+    cur.execute(
+        """SELECT 1 FROM "user" WHERE email = %s""",
+        (email,)
+        )
+    if (not cur.fetchone()):
+        cur.close()
+        conn.close()
+        raise HTTPException(status_code=400, detail="User not found")
+    
+    # Verify if the given password matches the stored password
+    cur.execute(
+        """SELECT password FROM "user" WHERE email = %s""",
+        (email,)
+        )
+    result = cur.fetchone()
+    db_hashed_password = result[0]
+    if (not pwd_context.verify(password, db_hashed_password)):
         raise HTTPException(status_code=401, detail="Incorrect password")
     
+    # Close connection to database
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    # If the login request is valid, generate and return token
     token = create_token(email)
     return {"token": token}
 
 @app.post("/signup")
-def login(request: SignupRequest):
-    if request.email in users_db:
-        raise HTTPException(status_code=400, detail="User already exists")
+def signup(request: SignupRequest):
+    # Connect to the database
+    conn = get_db_connection()
+    cur = conn.cursor()
 
+    # Extract email and password from request, hash the password
+    email = request.email
     hashed_password = pwd_context.hash(request.password)
-    users_db[request.email] = hashed_password
 
+    # Verify if the given email is already registered to an account
+    cur.execute(
+        """SELECT 1 FROM "user" WHERE email = %s""",
+        (email,)
+        )
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        raise HTTPException(status_code=400, detail="User already registered")
+
+    # If request is valid, insert email and password to the database
+    cur.execute(
+        """INSERT INTO "user" (email, password) VALUES (%s, %s)""",
+        (email, hashed_password)
+        )
+    
+    # Close connection to database
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    # Return success message
     return {"message": "Signup successful"}
 
 @app.post("/open_gate")
